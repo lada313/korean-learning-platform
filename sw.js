71163fa16be76743f6eb1b0736e1b926edc915e7
lada313 <78254005+lada313@users.noreply.github.com>
2025-07-21 01:25:19 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,24 +4,20 @@ const urlsToCache = [
   '/index.html',
   '/styles.css',
   '/script.js',
-  '/data/words.json',
-  '/data/levels.json',
-  '/assets/icon-192.png',
-  '/assets/icon-512.png'
+  '/icon-192.png',
+  '/icon-512.png'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache.filter(url => {
-          // Пропускаем несуществующие URL
-          try {
-            return new URL(url).pathname;
-          } catch (e) {
-            return false;
-          }
-        }));
-      })
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
