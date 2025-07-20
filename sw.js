f2311b07c40422764833eb38d32295d1cfa313ae
lada313 <78254005+lada313@users.noreply.github.com>
2025-07-21 01:35:25 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,31 @@
-const CACHE_NAME = 'korean-platform-v1';
+const CACHE_NAME = 'korean-platform-v3';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/styles.css',
-  '/script.js',
-  '/icon-192.png',
-  '/icon-512.png'
+  './',
+  './index.html',
+  './styles.css',
+  './script.js',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+      .catch(err => {
+        console.log('Cache addAll error:', err);
+      })
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
+      .then(response => {
+        return response || fetch(event.request);
+      })
   );
 });
