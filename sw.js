9a048087b341d711c055479df89bb63cfe86602a
lada313 <78254005+lada313@users.noreply.github.com>
2025-07-23 00:25:27 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,33 @@
-const CACHE_NAME = 'korean-trainer-v1';
+const CACHE_NAME = 'korean-platform-v4';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/styles.css',
-  '/script.js',
-  '/data/words.json',
-  '/icons/icon-192.png'
+  './',
+  './index.html',
+  './styles.css',
+  './script.js',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './data/words.json',
+  './data/levels.json'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache.map(url => new Request(url, {cache: 'reload'})));
+      })
+      .catch(err => {
+        console.log('Cache addAll error:', err);
+      })
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
+      .then(response => {
+        return response || fetch(event.request);
+      })
   );
 });
